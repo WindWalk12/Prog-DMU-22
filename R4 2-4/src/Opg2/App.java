13e9ff2f561cf990a27c01f6fe66cfcb17865dd6
WindWalk <nhygta@hotmail.com>
2022-09-09 12:01:33 +0200

    R4 2-4

---
+++ R4 2-4/src/Opg2/App.java
@@ -0,0 +1,13 @@
+package Opg2;
+
+public class App {
+    public static void main(String[] args) {
+        /*
+        You can't declare two intergers of the same name
+
+        int mystery = 1;
+        mystery = mystery + 1;
+        int mystery = 1 - 2 * mystery;
+        */
+    }
+}
---
+++ R4 2-4/src/Opg3/App.java
@@ -0,0 +1,7 @@
+package Opg3;
+
+public class App {
+    public static void main(String[] args) {
+        //Done in hand
+    }
+}
