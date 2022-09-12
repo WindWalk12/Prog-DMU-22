b40d806ff934cca300d0cc0e82257109a00ee485
WindWalk <nhygta@hotmail.com>
2022-09-12 08:41:07 +0200

    Fixed grammar

--- Person/src/PersonApp.java
+++ Person/src/PersonApp.java
@@ -2,7 +2,7 @@ public class PersonApp {
     public static void main(String[] args) {
         Person p1 = new Person("Jonas","Gertrudsvej 54", 2000, 2);
         p1.printPerson();
-        System.out.println("Has a yearly salary at " + p1.getYearlySalary());
+        System.out.println("Has a yearly salary of " + p1.getYearlySalary());
         System.out.println("");
         p1.newCompany();
         p1.printPerson();
