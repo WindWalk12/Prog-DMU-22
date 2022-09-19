cb4be55a32c3ed376933ee61f7127451960fef95
WindWalk12 <nhygta@hotmail.com>
2022-09-19 18:29:40 +0200

    Last of while loop workshop

---
+++ Workshop/src/Exercise5/App.java
@@ -0,0 +1,11 @@
+package Exercise5;
+
+import Exercise4.Gui3;
+import javafx.application.Application;
+
+public class App {
+
+	public static void main(String[] args) {
+		Application.launch(Gui.class);
+	}
+}
