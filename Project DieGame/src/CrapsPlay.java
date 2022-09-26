df6e0954b8362cd5f270ad90d10f4e076be02e7c
WindWalk <nhygta@hotmail.com>
2022-09-26 13:32:16 +0200

    Start of craps

---
+++ Project DieGame/src/CrapsPlay.java
@@ -0,0 +1,18 @@
+public class CrapsPlay {
+
+    private Die die1;
+    private Die die2;
+
+    public CrapsPlay() {
+        die1 = new Die();
+        die2 = new Die();
+    }
+
+    public void welcomeToGame() {
+
+    }
+
+    public void gameOver() {
+
+    }
+}
