3102c96c7b3c58e3eb8708026b7ba7bc40fd3f33
WindWalk12 <nhygta@hotmail.com>
2022-09-26 18:54:46 +0200

    Created the rest of CrapsPlay

--- Project DieGame/src/CrapsPlay.java
+++ Project DieGame/src/CrapsPlay.java
@@ -1,18 +1,77 @@
+import java.util.Scanner;
+
 public class CrapsPlay {
 
     private Die die1;
     private Die die2;
+    private Scanner scan;
+    private int countThrown = 0;
+    private boolean finished = false;
+    private boolean won = false;
+    private boolean thrown = false;
+    private int winningNumber = 0;
 
     public CrapsPlay() {
         die1 = new Die();
         die2 = new Die();
+        scan = new Scanner(System.in);
+    }
+
+    public int sumOfDices() {
+        return die1.getFaceValue() + die2.getFaceValue();
+    }
+
+    public void rollBothDices() {
+        die1.roll();
+        die2.roll();
     }
 
     public void welcomeToGame() {
-
+        System.out.println("Velkommen til Craps spillet");
     }
 
     public void gameOver() {
+        if (this.won) {
+            System.out.println("Du har vundet");
+        }
+        else {
+            System.out.println("Du har tabt");
+        }
+        scan.close();
+    }
 
+    private void takeTurn() {
+        rollBothDices();
+        System.out.println("Du har slået: " + sumOfDices());
+        System.out.println();
+        if (!thrown && (sumOfDices() == 7 || sumOfDices() == 11)) {
+            this.finished = true;
+            this.won = true;
+        }
+        else if (sumOfDices() == 2 || sumOfDices() == 3 || sumOfDices() == 12) {
+            this.finished = true;
+        }
+        else if (this.thrown && sumOfDices() == this.winningNumber) {
+            this.finished = true;
+            this.won = true;
+        } else if (!this.thrown) {
+            this.winningNumber = sumOfDices();
+            this.thrown = true;
+        }
+        this.countThrown++;
+    }
+
+    public void startGame() {
+        welcomeToGame();
+        while (!this.finished || this.countThrown == 7) {
+            System.out.print("Vil du kaste en terning? Angiv Ja eller Nej: ");
+            String goOn = scan.nextLine();
+            if (goOn.equalsIgnoreCase("Nej")) {
+                this.finished = true;
+            } else {
+                takeTurn();
+            }
+        }
+        gameOver();
     }
 }
---
+++ Project DieGame/src/CrapsPlayApp.java
@@ -0,0 +1,6 @@
+public class CrapsPlayApp {
+    public static void main(String[] args) {
+        CrapsPlay playCrapsPlay = new CrapsPlay();
+        playCrapsPlay.startGame();
+    }
+}
