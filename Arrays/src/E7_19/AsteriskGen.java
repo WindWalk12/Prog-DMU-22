845b62f2a822961cf9c90824769a4a55cb1e8433
WindWalk12 <nhygta@hotmail.com>
2022-10-02 16:10:25 +0200

    Fixed E7.19 and E7.22

--- Arrays/src/E7_19/AsteriskGen.java
+++ Arrays/src/E7_19/AsteriskGen.java
@@ -1,19 +1,36 @@
 package E7_19;
 
+import java.util.Scanner;
+
 public class AsteriskGen {
-    private int[] inputs = {3,4,7,4,44,39};
-    private final int maxValue = 40;
-    private int count = 1;
+    private int[] inputs;
+    private final int maxAsterisks = 40;
+    private Scanner scan;
+
+    public AsteriskGen() {
+        scan = new Scanner(System.in);
+        System.out.print("Number for size of array: ");
+        int size = scan.nextInt();
+        inputs = new int[size];
+        System.out.println("Write " + size + " numbers");
+        for (int i = 0; i < size; i++) {
+            int number = scan.nextInt();
+            inputs[i] = number;
+        }
+        System.out.println();
+    }
 
     public void Gen() {
+        int highestNumber = 0;
         for (int i = 0; i < inputs.length; i++) {
-            for (int j = 0; j < inputs[i]; j++) {
-                if (count <= maxValue) {
-                    System.out.print("*");
-                    count++;
-                }
+            if (inputs[i] > highestNumber) {
+                highestNumber = inputs[i];
             }
-            count = 1;
+        }
+        for (int i = 0; i < inputs.length; i++) {
+            for (int j = 0; j < Math.floor(maxAsterisks / (highestNumber/(float)inputs[i])); j++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
--- Arrays/src/E7_22/AsteriskGen.java
+++ Arrays/src/E7_22/AsteriskGen.java
@@ -1,21 +1,45 @@
 package E7_22;
 
+import java.util.Scanner;
+
 public class AsteriskGen {
-    private int[] inputs = {3,4,7,4,44,39};
-    private String[] countries = {"Denmark", "Germany", "Egypt", "US", "Canada", "Neverland"};
-    private final int maxValue = 40;
-    private int count = 1;
+    private int[] inputs;
+    private String[] captions;
+    private final int maxAsterisks = 40;
+    private Scanner scan;
+
+    public AsteriskGen() {
+        scan = new Scanner(System.in);
+        System.out.print("Number for size of array: ");
+        int size = scan.nextInt();
+        inputs = new int[size];
+        captions = new String[size];
+        System.out.println("Write " + size + " numbers");
+        for (int i = 0; i < size; i++) {
+            int number = scan.nextInt();
+            inputs[i] = number;
+        }
+        System.out.println();
+        System.out.println("Write " + size + " Strings");
+        for (int i = 0; i < size; i++) {
+            String caption = scan.next();
+            captions[i] = caption;
+        }
+        System.out.println();
+    }
 
     public void Gen() {
+        int highestNumber = 0;
         for (int i = 0; i < inputs.length; i++) {
-            System.out.print(countries[i] + " ");
-            for (int j = 0; j < inputs[i]; j++) {
-                if (count <= maxValue) {
-                    System.out.print("*");
-                    count++;
-                }
+            if (inputs[i] > highestNumber) {
+                highestNumber = inputs[i];
             }
-            count = 1;
+        }
+        for (int i = 0; i < inputs.length; i++) {
+            System.out.print(captions[i] + " ");
+            for (int j = 0; j < Math.floor(maxAsterisks / (highestNumber/(float)inputs[i])); j++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
