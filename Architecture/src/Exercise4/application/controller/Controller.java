dffacee1834f828943bad2c484c0a56b72148e33
WindWalk <nhygta@hotmail.com>
2022-11-07 11:50:38 +0100

    Done with all the Architecture exercises

---
+++ Architecture/src/Exercise4/application/controller/Controller.java
@@ -0,0 +1,137 @@
+package Exercise4.application.controller;
+
+import Exercise4.application.model.Company;
+import Exercise4.application.model.Employee;
+import Exercise4.storage.Storage;
+
+import java.util.ArrayList;
+
+public class Controller {
+    /**
+     * Creates a new Company.<br />
+     * Requires: hours >= 0.
+     */
+    public static Company createCompany(String name, int hours) {
+        Company company = new Company(name, hours);
+        Storage.addCompany(company);
+        return company;
+    }
+
+    /**
+     * Deletes the company.<br />
+     * Requires: The company has no employees.
+     */
+    public static void deleteCompany(Company company) {
+        Storage.removeCompany(company);
+    }
+
+    /**
+     * Updates the company.<br />
+     * Requires: hours >= 0.
+     */
+    public static void updateCompany(Company company, String name, int hours) {
+        company.setName(name);
+        company.setHours(hours);
+    }
+
+    /**
+     * Get all the companies
+     */
+    public static ArrayList<Company> getCompanies() {
+        return Storage.getCompanies();
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Creates a new employee.<br />
+     * Requires: wage >= 0.
+     */
+    public static Employee createEmployee(String name, int wage) {
+        Employee employee = new Employee(name, wage);
+        Storage.addEmployee(employee);
+        return employee;
+    }
+
+    /**
+     * Creates a new employee.<br />
+     * Requires: wage >= 0, company!=null.
+     */
+    public static Employee createEmployee(String name, int wage, Company company, int year) {
+        Employee employee = createEmployee(name, wage);
+        company.addEmployee(employee);
+        employee.setEmploymentYear(year);
+        return employee;
+    }
+
+    /**
+     * Deletes the employee.
+     */
+    public static void deleteEmployee(Employee employee) {
+        Company company = employee.getCompany();
+        if (company != null) {
+            company.removeEmployee(employee);
+        }
+        Storage.removeEmployee(employee);
+    }
+
+    /**
+     * Updates the employee.<br />
+     * Requires: wage >= 0.
+     */
+    public static void updateEmployee(Employee employee, String name, int wage, int year) {
+        employee.setName(name);
+        employee.setWage(wage);
+        employee.setEmploymentYear(year);
+    }
+    
+    /**
+     * Adds the employee to the company. Removes the employee from the old company if present.
+     */
+    public static void addEmployeeToCompany(Employee employee, Company company) {
+        company.addEmployee(employee);
+    }
+    
+    /**
+     * Removes the employee from the old company if not null.
+     * @param company The old company. Can be null.
+     * @param employee The employee to remove.
+     */
+    public static void removeEmployeeFromCompany(Employee employee, Company company) {
+        if (company != null) {
+            company.removeEmployee(employee);            
+        }
+    }
+
+    /**
+     * Get all the employees.
+     */
+    public static ArrayList<Employee> getEmployees() {
+        return Storage.getEmployees();
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Initializes the storage with some objects.
+     */
+    public static void initStorage() {
+        Company c1 = Controller.createCompany("IBM", 37);
+        Company c2 = Controller.createCompany("AMD", 40);
+        Controller.createCompany("SLED", 45);
+        Controller.createCompany("Vector", 32);
+
+        Controller.createEmployee("Bob Dole", 210, c2, 2011);
+        Controller.createEmployee("Alice Schmidt", 250, c1, 1995);
+        Controller.createEmployee("George Down", 150, c2, 2015);
+
+        Controller.createEmployee("Rita Uphill", 300);
+    }
+
+    public static void init() {
+
+        initStorage();
+
+    }
+
+}
